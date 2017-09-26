024d9bf4ad140635b55a71fa326d0f722c1ef352
mihailefter <mihai.lefter@gmail.com>
2017-09-26 12:53:09 +0200

    Support only for single allele in g. coordinates.

--- mutalyzer/nc_db.py
+++ mutalyzer/nc_db.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna, generic_protein
 
-
 from mutalyzer.GenRecord import PList, Locus, Gene, Record
+from mutalyzer.mapping import Converter
 
 url = make_url('postgresql://gbparser:parolamea@localhost/gbparser')
 
@@ -258,6 +258,7 @@ class Transcript(Base):
         """
         return self.get_reference()
 
+
 # Index('transcript_mapping_transcript',
 #       TranscriptMapping.accession, TranscriptMapping.version,
 #       TranscriptMapping.gene, TranscriptMapping.transcript,
@@ -405,6 +406,13 @@ def get_sequence_mmap(file_path, start, end):
 
 
 def get_reference(accession, version):
+    """
+    Retrieves the database reference entry for the user provided accession and
+    version.
+    :param accession: The accession from the user provided description.
+    :param version: The version number from the user provided description.
+    :return: Database entry.
+    """
     reference = Reference.query.filter_by(accession=str(accession), version=str(version)) \
         .order_by(Reference.id.asc()) \
         .first()
@@ -412,7 +420,24 @@ def get_reference(accession, version):
     return reference
 
 
-def get_record(record_id):
+def get_nc_record(record_id, description):
+    """
+    Get an NC record from the gbparser database and transform it into the
+    Mutalyzer record format.
+
+    :param record_id: HGVS format record description.
+    :param description: HGVS description object.
+    :return: The record in mutalyzer format or None if not found.
+    """
+    converter = Converter(assembly, output)
+
+    variant = description.RawVar
+    position_start = position_end = variant.StartLoc.PtLoc
+    if variant.EndLoc:
+        position_end = variant.EndLoc.PtLoc
+
+    print('first location {}'.format(position_start[0]))
+    print('last location {}'.format(position_end[0]))
 
     record = Record()
     accession = record_id.split('.')[0]
@@ -426,70 +451,37 @@ def get_record(record_id):
     record.source_version = reference.version
     record.organism = 'Homo sapiens'
     record.molType = 'g'
-    geneDict = {}
-    # transcripts = Transcript.query.filter_by(reference_id=reference.id).\
-    #     filter(Transcript.transcript_start > 1, Transcript.transcript_stop < 10000000).all()
-    db_transcripts = Transcript.query.filter_by(reference_id=reference.id).all()
-    # print("Begin transcript DB get loop time %s" % datetime.now())
-    # for transcript in db_transcripts:
-    #     if transcript.gene in geneDict:
-    #         gene = geneDict[transcript.gene]
-    #     else:
-    #         gene = Gene(transcript.gene)
-    #
-    #     if transcript.strand == '+':
-    #         gene.orientation = 1
-    #     if transcript.strand == '-':
-    #         gene.orientation = -1
-    #
-    #     version = gene.newLocusTag()
-    #     myTranscript = Locus(version)
-    #
-    #     myTranscript.mRNA = PList()
-    #     myTranscript.mRNA.location = [transcript.transcript_start,
-    #                                   transcript.transcript_stop]
-    #     myTranscript.CDS = PList()
-    #     myTranscript.CDS.location = [transcript.cds_start,
-    #                      transcript.cds_stop]
-    #     if transcript.exons and isinstance(transcript.exons, list):
-    #         for exon in transcript.exons:
-    #             myTranscript.CDS.positionList.append(exon.start)
-    #             myTranscript.CDS.positionList.append(exon.stop)
-    #     transcript_id = transcript.transcript_accession + '.' + transcript.transcript_version
-    #     myTranscript.transcriptID = transcript_id
-    #     myTranscript.proteinID = transcript.protein_accession + '.' + transcript.protein_version
-    #     myTranscript.linkMethod = 'ncbi'
-    #
-    #     gene.transcriptList.append(myTranscript)
-    #     geneDict[gene.name] = gene
-    # print("End transcript DB get loop time %s" % datetime.now())
 
-    print("%s: Begin local transcripts creation loop time" % datetime.now())
+    # Get the DB transcript entries.
+    db_transcripts = get_transcripts(accession, version, position_start[0], position_end[0])
+
+    # Extracting the transcripts from the DB entries.
     transcripts = []
     for transcript in db_transcripts:
-        myTranscript = {}
-        myTranscript['gene'] = transcript.gene
-        myTranscript['strand'] = transcript.strand
-        myTranscript['transcript_start'] = transcript.transcript_start
-        myTranscript['transcript_stop'] = transcript.transcript_stop
-        myTranscript['cds_start'] =transcript.cds_start
-        myTranscript['cds_stop'] = transcript.cds_stop
-        myTranscript['exons'] = []
+        my_transcript = {
+            'gene': transcript.gene,
+            'strand': transcript.strand,
+            'transcript_start': transcript.transcript_start,
+            'transcript_stop': transcript.transcript_stop,
+            'cds_start': transcript.cds_start,
+            'cds_stop': transcript.cds_stop,
+            'exons': [],
+            'transcriptID': transcript.transcript_accession + '.' + transcript.transcript_version,
+            'proteinID': transcript.protein_accession + '.' + transcript.protein_version,
+            'linkMethod': 'ncbi'
+        }
         if transcript.exons and isinstance(transcript.exons, list):
             for exon in transcript.exons:
                 exon = {'start': exon.start,
                         'stop': exon.stop}
-                myTranscript['exons'].append(exon)
-        myTranscript['transcriptID'] = transcript.transcript_accession + '.' + transcript.transcript_version
-        myTranscript['proteinID'] = transcript.protein_accession + '.' + transcript.protein_version
-        myTranscript['linkMethod'] = 'ncbi'
-        transcripts.append(myTranscript)
-    print("%s: End local transcripts creation loop time" % datetime.now())
+                my_transcript['exons'].append(exon)
+        transcripts.append(my_transcript)
 
-    print("%s: Begin transcript dict get loop time" % datetime.now())
+    # Generating the actual record entries in the Mutalyzer format.
+    gene_dict = {}
     for transcript in transcripts:
-        if transcript['gene'] in geneDict:
-            gene = geneDict[transcript['gene']]
+        if transcript['gene'] in gene_dict:
+            gene = gene_dict[transcript['gene']]
         else:
             gene = Gene(transcript['gene'])
 
@@ -499,49 +491,46 @@ def get_record(record_id):
             gene.orientation = -1
 
         version = gene.newLocusTag()
-        myTranscript = Locus(version)
+        my_transcript = Locus(version)
 
-        myTranscript.mRNA = PList()
-        myTranscript.mRNA.location = [transcript['transcript_start'],
-                                      transcript['transcript_stop']]
-        myTranscript.CDS = PList()
-        myTranscript.CDS.location = [transcript['cds_start'],
-                         transcript['cds_stop']]
+        my_transcript.mRNA = PList()
+        my_transcript.mRNA.location = [transcript['transcript_start'],
+                                       transcript['transcript_stop']]
+        my_transcript.CDS = PList()
+        my_transcript.CDS.location = [transcript['cds_start'],
+                                      transcript['cds_stop']]
         if transcript.get('exons') and isinstance(transcript.get('exons'), list):
             for exon in transcript['exons']:
-                myTranscript.CDS.positionList.append(exon['start'])
-                myTranscript.CDS.positionList.append(exon['stop'])
-        myTranscript.transcriptID = transcript['transcriptID']
-        myTranscript.proteinID = transcript['proteinID']
-        myTranscript.linkMethod = 'ncbi'
-        myTranscript.transcribe = True
+                my_transcript.CDS.positionList.append(exon['start'])
+                my_transcript.CDS.positionList.append(exon['stop'])
+        my_transcript.transcriptID = transcript['transcriptID']
+        my_transcript.proteinID = transcript['proteinID']
+        my_transcript.linkMethod = 'ncbi'
+        my_transcript.transcribe = True
 
-        gene.transcriptList.append(myTranscript)
-        geneDict[gene.name] = gene
-    print("%s: End transcript dict get loop time" % datetime.now())
+        gene.transcriptList.append(my_transcript)
+        gene_dict[gene.name] = gene
 
-    print("%s: Begin record.geneList time" % datetime.now())
-    record.geneList = list(geneDict.values())
-    print("%s: End record.geneList time" % datetime.now())
+    record.geneList = list(gene_dict.values())
 
+    # Get the sequewnce.
     seq_path = "/home/mlefter/projects/mutalyzer/data/gbparserout/" \
                + reference.checksum_sequence + '.sequence'
-    record.seq = Seq(get_sequence_mmap(seq_path, 1, reference.length+1), generic_dna)
+    record.seq = Seq(get_sequence_mmap(seq_path, 1, reference.length + 1), generic_dna)
+
     return record
 
 
-def get_transcripts(accession, version, position):
-    t_1 = time.time()
+def get_transcripts(accession, version, position_start, position_end):
     reference = get_reference(accession, version)
-    t_2 = time.time()
-    print(len(reference.transcripts))
 
-    position_s = int(position) - 100000
-    position_e = int(position) + 100000
+    p_s = int(position_start) - 100000
+    p_e = int(position_end) + 100000
 
-    transcripts = Transcript.query.filter_by(reference_id=reference.id).\
-        filter(Transcript.transcript_start > position_s, Transcript.transcript_stop < position_e).all()
-    t_3 = time.time()
-    print(t_2-t_1, t_3-t_2)
-
+    transcripts = Transcript.query.filter_by(reference_id=reference.id). \
+        filter(((Transcript.transcript_start <= p_s) & (p_s <= Transcript.transcript_stop)) |
+               ((Transcript.transcript_start <= p_e) & (p_e <= Transcript.transcript_stop)) |
+               ((Transcript.transcript_start >= p_s) & (p_e >= Transcript.transcript_stop))
+               ) \
+        .all()
     return transcripts
